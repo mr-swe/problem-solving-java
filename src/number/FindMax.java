86050d76f392d3bc85f03ac73846604647fcd80c
Mahfuzur Rahman <mahfuzur.rahman.swe@gmail.com>
2023-06-04 13:15:28 +0600

    find max change

--- src/number/FindMax.java
+++ src/number/FindMax.java
@@ -2,20 +2,23 @@ package number;
 
 public class FindMax {
 
-    public static void main(String[] args) {
-        int[] arr = {2, 3, 4, 1, 12, 5, 7, 11, 12};
+    static private void max() {
+        int[] arr = {2, 4, 10, 3, 10, 8, 10};
         int max = arr[0];
         int count = 0;
 
-        for (int j : arr) {
-            if (max == j) {
-                count++;
-            } else if (max < j) {
-                max = j;
-                count = 1;
+        for (int i = 0; i < arr.length; i++) {
+            if (max == arr[i]) {
+                count ++;
+            } else if (max < arr[i]) {
+                max = arr[i];
             }
         }
-        System.out.println("max is " + max + " and have " +  count);
+        System.out.println("Max number is: " + max + " and " + count + " times here");
+    }
+
+    public static void main(String[] args) {
+        FindMax.max();
     }
 
 }
